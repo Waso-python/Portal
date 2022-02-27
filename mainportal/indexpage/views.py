24880a174c06cedb921e9f2d03ee299913828408
sdarr <sdarr@student.21-school.ru>
2022-02-27 20:00:43 +0300

    second

--- mainportal/indexpage/views.py
+++ mainportal/indexpage/views.py
@@ -8,7 +8,7 @@ class IndexPageView(TemplateView):
     template_name = 'indexpage/index.html'
     
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({'key':'value'}) 
+        context = super().get_context_data(**kwargs) 
+        context.update({'key':'value'}) #content to send to template
         return context
     
