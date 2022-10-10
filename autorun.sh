31301af77ad3830f21dac7ac02c8cdfd5a1f051c
xdarov <xdarovv@gmail.com>
2022-10-10 12:22:17 +0300

    autorun

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-screen -L python3.10 home/Portal/mainportal/manage.py runserver &
-screen -L redis-server &
-sleep5 &&
-cd mainportal/ && screen -L celery -A mainportal worker --loglevel=info &
-screen -L celery -A mainportal beat --loglevel=info
+screen -d -m python3.10 /home/Portal/mainportal/manage.py runserver &
+screen -d -m redis-server &
+sleep 5 &&
+cd mainportal/ && screen -d -m celery -A mainportal worker --loglevel=info &
+screen -d -m celery -A mainportal beat --loglevel=info
