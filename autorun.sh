c1f67f21a911f01d96b884909de562355fac2b0e
xdarov <xdarovv@gmail.com>
2022-10-10 12:50:42 +0300

    dickerfile complete

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-screen -d -m python3.10 /home/Portal/mainportal/manage.py runserver &
+screen -d -m python3.10 /home/Portal/mainportal/manage.py runserver 0.0.0.0:9000 &
 screen -d -m redis-server &
 sleep 5 &&
 cd mainportal/ && screen -d -m celery -A mainportal worker --loglevel=info &
-screen -d -m celery -A mainportal beat --loglevel=info
+cd mainportal/ && screen -d -m celery -A mainportal beat --loglevel=info
